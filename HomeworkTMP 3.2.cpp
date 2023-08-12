1f227c16648fd9336e58cea1f6a81937f2e29fe7
Даниил <derendervad@gmail.com>
2023-08-12 19:22:54 +0300

    change

--- HomeworkTMP 3.2.cpp
+++ HomeworkTMP 3.2.cpp
@@ -2,35 +2,47 @@
 #include <vector>
 #include <fstream>
 
-class Observed {
+class Observer {
+private:
+    std::vector<Observer*> observers_;
 public:
-    virtual void onWarning(const std::string& message) {}
-    virtual void onError(const std::string& message) {}
-    virtual void onFatalError(const std::string& message) {}
-};
+    void AddObserver(Observer* obs) {
+        observers_.push_back(obs);
+    }
 
-class DecoderObserved : public Observed {
-protected:
-    std::vector<Observed*> obs_;
-    Observed* obs2;
-    explicit DecoderObserved(Observed* obs) : obs2(obs) { obs_.push_back(obs2); }
-    void RemoveObserver(Observed* obs) {
-        auto it = std::remove(obs_.begin(), obs_.end(), obs);
-        obs_.erase(it, obs_.end());
+    virtual void onWarning(const std::string& message) {
+        for (int i = 0; i < observers_.size(); i++) {
+            observers_[i]->onWarning(message);
+        }
+    }
+
+    virtual void onError(const std::string& message) {
+        for (int i = 0; i < observers_.size(); i++) {
+            observers_[i]->onError(message);
+        }
+    }
+
+    virtual void onFatalError(const std::string& message) {
+        for (int i = 0; i < observers_.size(); i++) {
+            observers_[i]->onFatalError(message);
+        }
+    }
+
+    void RemoveObserver(Observer* obs) {
+        auto it = std::remove(observers_.begin(), observers_.end(), obs);
+        observers_.erase(it, observers_.end());
     }
 };
 
-class ForOnWarning : public DecoderObserved {
+class ForOnWarning : public Observer {
 public:
-    explicit ForOnWarning(Observed* obs) : DecoderObserved(obs) {}
     void onWarning(const std::string& message) override {
         std::cout << "Warning: " << message << std::endl;
     }
 };
 
-class ForOnError : public DecoderObserved {
+class ForOnError : public Observer {
 public:
-    explicit ForOnError(Observed* obs) : DecoderObserved(obs) {}
     void onError(const std::string& message) override {
         std::ofstream out("Error.txt", std::ios::app);
         if (out.is_open()) {
@@ -40,9 +52,8 @@ public:
     }
 };
 
-class ForOnFatalError : public DecoderObserved {
+class ForOnFatalError : public Observer {
 public:
-    explicit ForOnFatalError(Observed* obs) : DecoderObserved(obs) {}
     void onFatalError(const std::string& message) override {
         std::cout << "FatalError: " << message << std::endl;
         std::ofstream out("Error.txt", std::ios::app);
@@ -51,32 +62,16 @@ public:
         }
         out.close();
     }
- };
-
-class Realization : public DecoderObserved {
-public:
-    explicit Realization(Observed* obs) : DecoderObserved(obs) {}
-    void onWarning(Observed* obs, const std::string& message) {
-        obs->onWarning(message);
-    }
-    void onError(Observed* obs, const std::string& message) {
-        obs->onError(message);
-    }
-    void onFatalError(Observed* obs, const std::string& message) {
-        obs->onFatalError(message);
-    }
 };
 
 int main()
 {
-    auto war = new ForOnWarning(new Observed);
-    auto err = new ForOnError(new Observed);
-    auto fat = new ForOnFatalError(new Observed);
+    auto observers = new Observer();
+    auto war = new ForOnWarning(); observers->AddObserver(war);
+    auto err = new ForOnError(); observers->AddObserver(err);
+    auto fal = new ForOnFatalError(); observers->AddObserver(fal);
 
-    auto realiz = new Realization(new Observed);
-
-    realiz->onWarning(war, "check 1");
-    realiz->onError(err, "check 2");
-    realiz->onFatalError(fat, "check 3");
-    return 0;
+    observers->onWarning("Check 1");
+    observers->onError("Check 2");
+    observers->onFatalError("Check 3");
 }
